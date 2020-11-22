f59823a965d96b5e8c0e090c7c0a17fff7c35ca7
trevoramaco <32376098+trevoramaco@users.noreply.github.com>
2020-11-22 03:16:45 -0500

    Network changes, player movement changes

---
+++ Assets/Code/ThirdPersonMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class ThirdPersonMovement : NetworkBehaviour
+{
+    public CharacterController controller;
+    Transform cam;
+
+    public float speed = 5f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    private void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer) { return; }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float verticle = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, verticle).normalized;
+
+        if(direction.magnitude >= 0.1)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Code/UIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Escape quits game...
+        // TODO: pressing escape brings up UI menu with options
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+
+#else
+        Application.Quit();
+#endif
+        }
+    }
+}
